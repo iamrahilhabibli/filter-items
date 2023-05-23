f015020014c2e19e334d775f912772e0eec0c9ad
= <=>
2023-05-23 20:08:28 +0400

    render/generate func added

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const apartmentsContainer = document.querySelector(".apartments__container");
+
 const minprice = document.querySelector("#minprice");
 const maxprice = document.querySelector("#maxprice");
 const minSpan = document.querySelector("minSpan");
@@ -5,3 +7,22 @@ const minSpan = document.querySelector("minSpan");
 minprice.addEventListener("change", (e) => {
   console.log(e.target.value);
 });
+
+function renderApartments() {
+  apartments.forEach((apartment) => {
+    const apartmentCard = generateApartment(apartment);
+    apartmentsContainer.appendChild(apartmentCard);
+  });
+}
+
+function generateApartment(apartments) {
+  const card = document.createElement("div");
+  card.innerHTML = `  <div class="card">
+  <span>ID: <span class="id">${apartments.id}</span></span>
+  <span>Price: <span class="price">${apartments.price}</span></span>
+  <span>Area: <span class="area">${apartments.area}</span></span>
+  <span>Rooms: <span class="rooms">${apartments.rooms}</span> </span>
+</div>`;
+  return card;
+}
+renderApartments();
